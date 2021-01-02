c32207a671d3f5256e1702c014fcfd576b2dda04
Junlin Chow <jameszhou2108@hotmail.com>
2021-01-02 10:31:27 +0800

    refine: doc

--- 7.整数反转.java
+++ 7.整数反转.java
@@ -10,6 +10,7 @@ class Solution {
         int eff = x > 0 ? 1 : -1;
         int result = 0;
         for(x = Math.abs(x); x > 0; x /= 10) {
+            // result * 10 + x % 10 > Integer.MAX_VALUE
             if (result > (Integer.MAX_VALUE - x % 10) / 10) {
                 return 0;
             }
